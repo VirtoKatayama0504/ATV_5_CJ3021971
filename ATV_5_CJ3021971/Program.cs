9766d46f9c86174fd69e430f1cdc0912b07e096c
CJ3021971 <CJ3021971@LAB03-PC14>
2024-03-27 12:22:15 -0300

    Adicionar arquivos de projeto.

---
+++ ATV_5_CJ3021971/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ATV_5_CJ3021971
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int soma = 0; int temp;
+            for(int n = 0; n <= 3; n++)
+            {
+                Console.WriteLine("Digite um valor: ");
+                temp = int.Parse(Console.ReadLine());
+                if (temp < 0)
+                {
+                    soma = soma + temp;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("A media é {0}", temp / 3);
+                }
+
+            }
+
+            int nx = 0;
+            Console.WriteLine("Exercicio 1");
+            Console.WriteLine("Digite um numero inteiro");
+            int nz = int.Parse(Console.ReadLine());
+            for (nx = 0; nx <= nz; nx++)
+            {
+                Console.WriteLine(nx);
+            }
+
+            int nq = 0;
+            Console.WriteLine("Exercicio 2");
+            Console.WriteLine("Digite um numero inteiro");
+            int nm = int.Parse(Console.ReadLine());
+            for  (nq = 0; nq <= nm; nq++)
+            {
+                if (nq % 2 == 0)
+                {
+                    Console.WriteLine(nq);
+
+                }
+            }
+
+            int div = 1000;
+            Console.WriteLine("Exercicio 3");
+            Console.WriteLine("Digite um numero inteiro menor que 1000");
+            int mil = int.Parse(Console.ReadLine());
+            if (mil < 1000)
+            {
+                for (div = 1000; div >= mil; div--)
+                {
+                  if (div % 2 == 0)
+                    {
+                        Console.WriteLine(div);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("oPÇÃO INVALIDA");
+            }
+
+            Console.WriteLine("Exercicio 5");
+            int aq, divx;
+            Console.WriteLine("Digite um numero");
+            aq = int.Parse(Console.ReadLine());
+            for (divx = 1; divx <= aq; divx++)
+            {
+                if (aq %divx == 0)
+                {
+                    Console.WriteLine(divx);
+                }
+            }
+            */
+
+            Console.WriteLine("Exercicio 6");
+
+
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+  
